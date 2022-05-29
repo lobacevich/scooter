9d714fa1f4c24045867b7738f3978c739b1054fc
lobacevich <lobacevich_@tut.by>
2022-05-29 19:38:02 +0300

    Initial commit

---
+++ src/test/java/by/senla/training/lobacevich/scooter/ScooterRentApplicationTests.java
@@ -0,0 +1,13 @@
+package by.senla.training.lobacevich.scooter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScooterRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
